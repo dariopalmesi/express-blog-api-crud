866e5806fbe113c0fb9f38ffefb7d671012b9c05
dariopalmesi <dariopalmesi88@gmail.com>
2024-11-06 15:02:53 +0100

    initial bonus commit

---
+++ bonus/db/db.js
@@ -0,0 +1,38 @@
+const animeList = [
+    {
+      id: 1,
+      titolo: "Naruto",
+      genere: ["Azione", "Avventura"],
+      anno: 2002,
+      episodi: 220
+    },
+    {
+      id: 2,
+      titolo: "Attack on Titan",
+      genere: ["Azione", "Fantasy", "Drammatico"],
+      anno: 2013,
+      episodi: 87
+    },
+    {
+      id: 3,
+      titolo: "One Piece",
+      genere: ["Azione", "Avventura", "Commedia"],
+      anno: 1999,
+      episodi: 1000
+    },
+    {
+      id: 4,
+      titolo: "Death Note",
+      genere: ["Mistero", "Thriller", "Soprannaturale"],
+      anno: 2006,
+      episodi: 37
+    },
+    {
+      id: 5,
+      titolo: "My Hero Academia",
+      genere: ["Azione", "Supereroi"],
+      anno: 2016,
+      episodi: 113
+    }
+  ];
+  
